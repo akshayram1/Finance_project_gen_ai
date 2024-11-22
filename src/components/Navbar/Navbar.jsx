bae9f0af8167e9a4c23ad18556f8a89e3b41f0ae
akshayram1 <akshaychame2@gmail.com>
2024-11-22 09:16:45 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,13 +7,14 @@ import { FiPlus } from "react-icons/fi";
 
 import './Navbar.scss'
 import Input from '../Input/Input';
+import { color } from 'chart.js/helpers';
 
 const Navbar = () => {
     const { user } = useAuthContext();
-    const [ showModal, setShowModal ] = useState(false)
-    const [ openDropdown, setOpenDropdown ] = useState(false)
-    const [ modalTitle, setModalTitle ] = useState()
-    const [ inputCategory, setInputCategory ] = useState()
+    const [showModal, setShowModal] = useState(false)
+    const [openDropdown, setOpenDropdown] = useState(false)
+    const [modalTitle, setModalTitle] = useState()
+    const [inputCategory, setInputCategory] = useState()
 
     const handleOpenModal = () => {
         setShowModal(true)
@@ -25,7 +26,7 @@ const Navbar = () => {
 
     const dropdownRef = useRef()
     const closeOutDropdown = e => dropdownRef.current === e.target ? setOpenDropdown(false) : ''
-    
+
     const sendInfo = (title, category) => {
         setModalTitle(title)
         setInputCategory(category)
@@ -34,45 +35,38 @@ const Navbar = () => {
     return (
         <>
             <div className='navbar'>
-               
-                <NavLink className={'logo'} to='/'>
-               {/*  <FiBarChart style={{ marginRight: '1rem'}}/> */}
-                EduFinance Hub
+                <NavLink className='logo' to='/'>
+                    EduFinance Hub
                 </NavLink>
                 <div className='flexy'>
-                    {user && <button className='add-btn' onClick={() => setOpenDropdown(!openDropdown)}> <FiPlus/></button>}
-                    {user ? 
-                    (
-                        <NavLink to='/profile' className='flexy navUser'>
-                           {/*  <div className='frame-img'>
-                            <img src={user.avatar} alt="user avatar" />
-                            </div> */}
-                            <p className='text-4xl'>Hi, {user.name}</p> 
-                        </NavLink>
-                    )
-                    :
-                    (<div className='flexy'>
-                        <NavLink to='/home'>Home</NavLink>
-                        <NavLink to='/login'>Login</NavLink>
-                        <NavLink to='/register'>Register</NavLink>
-                    </div>)
+                    {user && <button className='add-btn' onClick={() => setOpenDropdown(!openDropdown)}> <FiPlus /></button>}
+                    {user ?
+                        (
+                            <NavLink to='/profile' className='flexy navUser'>
+                                <p className='text-4xl'>Hi, {user.name}</p>
+                            </NavLink>
+                        )
+                        :
+                        (<div className='flexy'>
+                            <NavLink to='/home' className='nav-link'>Home</NavLink>
+                            <NavLink to='/login' className='nav-link'>Login</NavLink>
+                            <NavLink to='/register' className='nav-link'>Register</NavLink>
+                        </div>)
                     }
-                   {/*  {user && <button className='button-out' onClick={()=>logout()}>Logout</button>} */}
                 </div>
             </div>
-            {showModal && <Modal onClose={handleCloseModal} title={modalTitle}> <Input onClose={handleCloseModal} sector={inputCategory}/></Modal>}
+            {showModal && <Modal onClose={handleCloseModal} title={modalTitle}> <Input onClose={handleCloseModal} sector={inputCategory} /></Modal>}
             {openDropdown && (
                 <div className='drop-screen' ref={dropdownRef} onClick={closeOutDropdown}>
                     <div className='dropdown'>
-                        <button className='dropdown-item' onClick={() => {handleOpenModal(); sendInfo('Add Expense', 'expense')}}>Expense</button>
-                        <button className='dropdown-item' onClick={() => {handleOpenModal(); sendInfo('Add Income', 'income')}}>Income</button>
+                        <button className='dropdown-item' onClick={() => { handleOpenModal(); sendInfo('Add Expense', 'expense') }}>Expense</button>
+                        <button className='dropdown-item' onClick={() => { handleOpenModal(); sendInfo('Add Income', 'income') }}>Income</button>
                         <Link to='/goals' className='dropdown-item' onClick={() => setOpenDropdown(!openDropdown)}>Goal</Link>
                     </div>
                 </div>
-                )}
+            )}
         </>
-
-    )
+    );
 }
 
 export default Navbar
